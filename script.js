76c20ab8eb4fb52b946fa51a61d48a267c39df10
Maria de Fatima de Jesus Souza <00001106914831SP@al.educacao.sp.gov.br>
2024-09-27 19:20:54 -0300

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-    button.addEventListener('click', function(){
-        const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+    button.addEventListener('click', function() {
+        const atual = document.querySelector('.passo.ativo'); // Elemento com o passo atual
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo'); // Próximo passo
 
+        // Esconder o passo atual
         if (atual) {
             atual.classList.remove('ativo');
         }
 
+        // Mostrar o próximo passo
         const proximoElemento = document.getElementById(proximoPasso);
         if (proximoElemento) {
             proximoElemento.classList.add('ativo');
         } else {
             console.error('Próximo passo não encontrado: ', proximoPasso);
         }
-    })
+    });
 });
